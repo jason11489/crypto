0f1af57ff94113df4b48d0ab0b38c547b2d47163
kyup0101 <85879831+kyup0101@users.noreply.github.com>
2021-07-26 16:26:57 +0900

    Create pohlig_hellman.py

---
+++ pohlig_hellman.py
@@ -0,0 +1,21 @@
+import Exponentiation
+import Prime_Power_Order
+import CRT
+
+q = 11251
+n = 625
+npe = [(5,4)]
+g = 5448
+h = 6909
+
+xk = []
+for j in range(0,len(npe)):
+    pnej = n//Exponentiation.mns1(npe[j][0],npe[j][1])
+    gj = Exponentiation.mns(g,pnej,q)
+    hj = Exponentiation.mns(h,pnej,q)
+    xj = ((Prime_Power_Order.ppo(q,npe[j][0],npe[j][1],gj,hj)),Exponentiation.mns1(npe[j][0],npe[j][1]))
+    xk.append(xj)
+
+root = CRT.crt(xk)
+
+print("x =", root, "(", Exponentiation.mns(g,root,q) == h, ")")
