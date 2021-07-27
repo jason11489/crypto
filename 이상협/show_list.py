52314b0fe8b185348d90b5a698b200f73d816adf
kyup0101 <85879831+kyup0101@users.noreply.github.com>
2021-07-27 21:23:36 +0900

    Delete 이상협 directory

--- 이상협/show_list.py
+++
@@ -1,8 +0,0 @@
-def show_list(L):
-    cnt = 0
-    while cnt < len(L):
-        print("{}, ".format(L[cnt]), end = '')
-        cnt = cnt + 1
-        if(cnt%5 == 0):
-            print("\n", end = '')
-    print("\n")
